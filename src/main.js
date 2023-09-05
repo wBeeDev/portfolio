7737c2dee1afe796f250b775056a486d7bc6dbb2
wBeeDev <hiryuji@kakao.com>
2023-09-05 20:46:17 +0900

    JS: Home

--- src/main.js
+++ src/main.js
@@ -1,10 +1,17 @@
 // Header에 페이지 스크롤시 다크 스타일링 적용
 const header = document.querySelector(".header");
-const headerHeight = header.getBoundingClientRect().height;
+const headerHeight = header.offsetHeight;
 document.addEventListener("scroll", () => {
   if (window.scrollY > headerHeight) {
     header.classList.add("header--dark");
   } else {
     header.classList.remove("header--dark");
   }
+});
+
+// Home 섹션을 아래로 스크롤시 투명하게 처리
+const home = document.querySelector(".home__container");
+const homeHeight = home.offsetHeight;
+document.addEventListener("scroll", () => {
+  home.style.opacity = 1 - window.scrollY / homeHeight;
 });
