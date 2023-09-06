ee75ce62fb4ffd4532720bca8e1652ad22c501d3
wBeeDev <hiryuji@kakao.com>
2023-09-06 10:54:06 +0900

    JS: Projects

--- src/projects.js
+++ src/projects.js
@@ -1,1 +1,36 @@
 "use strict";
+
+// 프로젝트 필터링 관련 로직처리
+const categories = document.querySelector(".categories");
+const projects = document.querySelectorAll(".project");
+const projectsContainer = document.querySelector(".projects");
+categories.addEventListener("click", (e) => {
+  const filter = e.target.dataset.category;
+  if (filter == null) {
+    return;
+  }
+  handleActiveSelection(e.target);
+  filterProjects(filter);
+});
+
+function handleActiveSelection(target) {
+  // Active 메뉴 재설정
+  const active = document.querySelector(".category--selected");
+  active.classList.remove("category--selected");
+  target.classList.add("category--selected");
+}
+
+function filterProjects(filter) {
+  // 프로젝트 필터링
+  projects.forEach((project) => {
+    if (filter === "all" || filter === project.dataset.type) {
+      project.style.display = "block";
+    } else {
+      project.style.display = "none";
+    }
+  });
+  projectsContainer.classList.add("anim-out");
+  setTimeout(() => {
+    projectsContainer.classList.remove("anim-out");
+  }, 200);
+}
